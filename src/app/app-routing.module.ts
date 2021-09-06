f455ad396c22db8bdfc40af7247ac88ad3e242e4
Fábio Roberto Ferreira <fabiorobertoferreira@gmail.com>
2021-09-06 12:07:27 -0300

    primeiro commit projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {CardComponent} from './card/card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'card', component: CardComponent }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
