8f71ec55653951f740585e1b0e7c1f7a92fc9886
Fábio Roberto Ferreira <fabiorobertoferreira@gmail.com>
2021-09-07 11:04:54 -0300

    final

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# ProjectsEldorado
+# Projecto Eldorado
+
+Utilizar npm install  e ng serve para rodar a aplicação.
+
+Projeto como teste.
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.4.
 
---
+++ src/app/home/cards-others.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cards-others',
+  templateUrl: './cards-others.component.html',
+  styleUrls: ['./cards-others.component.scss']
+})
+export class CardsOthersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
